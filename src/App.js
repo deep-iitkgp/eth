46c3fe51762c2a309b3d911ed7489ff2294e81bb
Deep2499 <106651757+Deep2499@users.noreply.github.com>
2023-04-16 10:33:52 +0530

    second commit

--- src/App.js
+++ src/App.js
@@ -5,17 +5,13 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://eth-ex.netlify.app/"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          eth-ex
         </a>
       </header>
     </div>
